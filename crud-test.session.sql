8cdd72a7ccfa680d303f3676528e87ecefed0971
freshcodeAlice <freshjs.ofte@gmail.com>
2022-12-12 09:33:00 +0200

    init

---
+++ crud-test.session.sql
@@ -0,0 +1,8 @@
+CREATE TABLE things(
+    id serial PRIMARY KEY,
+    updated_at timestamp NOT NULL DEFAULT current_timestamp,
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    body text NOT NULL CHECK (body!= '')
+);
+
+---INSERT INTO things (body) VALUES ('Hello, backend!');
